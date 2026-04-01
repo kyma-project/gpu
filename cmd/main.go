1dc50fb6c22d64c1db05e6c8535a72b37ce95576
Vanja Radic <vanja.radic02@sap.com>
2026-04-01 12:25:20 +0200

    feat: add Gpu CRD for SKR API group

--- cmd/main.go
+++ cmd/main.go
@@ -34,6 +34,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	gpuv1beta1 "github.com/kyma-project/gpu/api/gpu/v1beta1"
+	gpucontroller "github.com/kyma-project/gpu/internal/controller/gpu"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -45,6 +48,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(gpuv1beta1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -173,6 +177,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err := (&gpucontroller.GpuReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "Failed to create controller", "controller", "Gpu")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
